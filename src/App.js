6e617bf09ea77ff4ac3d29b591b4414cfb3b0e20
Russell Telen <rtelen@my.bcit.ca>
2020-11-29 20:52:15 -0800

    Clean up

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Photo Portfolio</h1>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
