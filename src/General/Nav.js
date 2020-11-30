a54792fdb4f4c7d24eada640827c128dc8c882ef
Russell Telen <rtelen@my.bcit.ca>
2020-11-29 21:03:55 -0800

    Set up NavBar

--- src/General/Nav.js
+++ src/General/Nav.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <div>
-      <h1>Nav works</h1>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item px-4">
+              <NavLink to="/" exact className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item px-4">
+              <NavLink to="/projects" className="nav-link">
+                Film Diaries
+              </NavLink>
+            </li>
+            <li className="nav-item px-4">
+              <NavLink to="/about" className="nav-link">
+                About Me
+              </NavLink>
+            </li>
+            <li className="nav-item px-4">
+              <NavLink to="/contact" className="nav-link">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
