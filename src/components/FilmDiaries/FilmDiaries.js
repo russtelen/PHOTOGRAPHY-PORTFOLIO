4912550302fe870f8f2b8581646e32bf9d5ed414
russtelen <rtelen@my.bcit.ca>
2020-12-01 20:21:22 -0800

    Add film diaries sample img

--- src/components/FilmDiaries/FilmDiaries.js
+++ src/components/FilmDiaries/FilmDiaries.js
@@ -4,6 +4,12 @@ const FilmDiaries = () => {
   return (
     <div className="filmdiaries">
       <h1 className="text-center">Film Diaries</h1>
+      <div className="container">
+        <img
+          className="film-img"
+          src="https://russtelen-photoportfolio.s3.ca-central-1.amazonaws.com/filmdiaries.jpg"
+        />
+      </div>
     </div>
   );
 };
