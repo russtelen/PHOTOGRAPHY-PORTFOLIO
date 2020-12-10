4e95a0a142eca5f5b376a4b738a345097b0150d7
Russell Telen <rtelen@my.bcit.ca>
2020-12-10 06:35:46 -0800

    Add animations to FilmDiaries #1

--- src/components/FilmDiaries/FilmDiaries.js
+++ src/components/FilmDiaries/FilmDiaries.js
@@ -3,8 +3,10 @@ import React from "react";
 const FilmDiaries = () => {
   return (
     <div className="filmdiaries">
-      <h1 className="text-center">Film Diaries</h1>
-      <div className="container">
+      <h1 className="text-center animate__animated animate__bounceIn">
+        Film Diaries
+      </h1>
+      <div className="container text-center animate__animated animate__fadeIn">
         <img
           className="film-img"
           src="https://russtelen-photoportfolio.s3.ca-central-1.amazonaws.com/filmdiaries.jpg"
