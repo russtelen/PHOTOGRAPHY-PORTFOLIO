aa38d21fb24afdc97c7332fcb499095e178be591
Russell Telen <rtelen@my.bcit.ca>
2020-12-10 06:16:51 -0800

    Add animations to Landing #1

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -5,15 +5,17 @@ const Landing = () => {
   return (
     <div className="landing">
       <div className="jumbotron-custom">
-        <h1 className="display-4 text-light">Russ Telen</h1>
-        <p className="lead text-light">Film Photographer</p>
-        <hr className="my-4" />
-        <p className="text-light">Vancouver, BC</p>
-        <p className="lead">
-          <NavLink className="landing-button" to="/filmdiaries">
-            See My Work
-          </NavLink>
-        </p>
+        <div className="animate__animated animate__fadeInDown animate__fast">
+          <h1 className="display-4 text-light ">Russ Telen</h1>
+          <p className="lead text-light">Film Photographer</p>
+          <hr className="my-4" />
+          <p className="text-light">Vancouver, BC</p>
+          <p className="lead">
+            <NavLink className="landing-button" to="/filmdiaries">
+              See My Work
+            </NavLink>
+          </p>
+        </div>
       </div>
     </div>
   );
