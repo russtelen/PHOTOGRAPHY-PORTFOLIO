5cd30aa203596158311feb3c7df350532d8387e6
Russell Telen <rtelen@my.bcit.ca>
2020-12-31 02:12:53 -0800

    Style and Add Contact contents #2

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineGlobal,
+  AiOutlineGithub,
+} from "react-icons/ai";
 
 const Contact = () => {
   return (
@@ -7,41 +13,81 @@ const Contact = () => {
         Get In Touch
       </h1>
 
-      <div className="container mb-5">
-        <form className="contact__form" name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">Name</label>
-            <input type="text" className="form-control" name="name" required />
+      <div className="container my-5">
+        <div className="row">
+          <div className="container col-lg-6 mb-5">
+            <div>
+              <p className="h4">Email:</p>
+              <p>russelltelen@gmail.com</p>
+            </div>
+            <div>
+              <p className="h4">Phone:</p>
+              <p>778-869-2774</p>
+            </div>
+            <div>
+              <p className="h4">Follow:</p>
+              <div className="row">
+                <a
+                  href="https://www.instagram.com/russtelen/"
+                  className="col-2 "
+                >
+                  <AiOutlineInstagram size={45} />
+                </a>
+                <a className="col-2 ">
+                  <AiOutlineFacebook size={45} />
+                </a>
+                <a className="col-2 " href="https://www.russtelen.me/">
+                  <AiOutlineGlobal size={45} />
+                </a>
+                <a className="col-2 " href="https://github.com/russtelen">
+                  <AiOutlineGithub size={45} />
+                </a>
+              </div>
+            </div>
           </div>
 
-          <div className="form-group">
-            <label htmlFor="exampleFormControlInput1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              name="email"
-              required
-            />
-          </div>
+          <div className="container col-lg-6">
+            <form className="contact__form" name="contact" method="post">
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="form-group">
+                <label htmlFor="exampleFormControlInput1">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  required
+                />
+              </div>
 
-          <div className="form-group">
-            <label htmlFor="exampleFormControlTextarea1">Message</label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              name="message"
-              rows="10"
-              required
-            ></textarea>
-          </div>
+              <div className="form-group">
+                <label htmlFor="exampleFormControlInput1">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  required
+                />
+              </div>
 
-          <div className="text-center">
-            <button type="submit" className="btn btn-sm btn-light">
-              Send Message
-            </button>
+              <div className="form-group">
+                <label htmlFor="exampleFormControlTextarea1">Message</label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  name="message"
+                  rows="10"
+                  required
+                ></textarea>
+              </div>
+
+              <div className="text-center">
+                <button type="submit" className="btn btn-sm btn-light">
+                  Send Message
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
